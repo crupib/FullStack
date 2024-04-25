81fc515b0dafb02a52abfb56df4f633fc8d7e596
crupib <crupib@gmail.com>
2024-04-24 20:25:36 -0400

    cars

---
+++ cars/src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  let data = ["Fiat", "Peugeot","Ford","Renault","Citroen"]
+  return (
+    <div className="App max-w-3xl mx-auto h-full">
+     <h1 className="bg-slate-500 text-white text-center 
+       ">This is a Tailwind styled site!</h1>
+      <div>
+        {data.map(
+          (el)=>{
+            return <div>Cars listed as <span className=
+              "font-bold">{el.toUpperCase()}</span></div>
+          }
+        )}
+      </div>
+    </div>
+  );
+}
+export default App;
